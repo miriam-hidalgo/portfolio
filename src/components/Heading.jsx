d608576af61aa9c2b583b7957576ad65ca27d43f
Miri.Hid <mirihdz11@gmail.com>
2019-07-21 22:34:27 -0400

    Updated lodash.template

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -8,27 +8,17 @@ function Heading() {
   return (
 <div className="jumbotron jumbotron-fluid">
   <div className="container">
-
-
     <Row>
     <Col xs={6} md={4}>
     <Image src={photo} roundedCircle thumbnail />
     </Col>
-
     <Col xs={6} md={8}>
       <h1 className="display-4">Hello, I am Miriam Hidalgo</h1>
       <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     </Col>
-
   </Row>
-    
-
-
   </div>
 </div>
-  
-  
- 
   );
 }
 
